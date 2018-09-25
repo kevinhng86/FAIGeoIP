040827e0ffcddac24a6ad6d1d8cf7d65d916ac06
kevinhng86 <kevinhng86@gmail.com>
2018-09-24 18:32:31 -0700

    Update kill.go

--- FaiGeoIP/kill.go
+++ FaiGeoIP/kill.go
@@ -8,7 +8,7 @@
 #    of this software and associated documentation files (the "Software"), to deal        #
 #    in the Software without restriction, including without limitation the rights         #
 #    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies     #
-#    of the Software, and to permit persons to whom the Software is furnished             # 
+#    of the Software, and to permit persons to whom the Software is furnished             #
 #    to do so, subject to the following conditions:                                       #
 #                                                                                         #
 #    The above copyright notice and this permission notice shall be  included in all      #
@@ -31,67 +31,67 @@
 package FaiGeoIP
 
 import(
-	"os"
-	"errors"
-	"strconv"
-	"syscall"
+    "os"
+    "errors"
+    "strconv"
+    "syscall"
 )
 
 func Kill(pidFile string) (bool, error) {
-    if _, err := os.Stat(pidFile); os.IsNotExist(err) {
+     if _, err := os.Stat(pidFile); os.IsNotExist(err) {
         // PID File is not exist
-	return true, errors.New("PID file is not existed. The program is probably not running.")
+        return true, errors.New("PID file is not existed. The program is probably not running.")
     } else {
-	// PID File Exist.
-	f, err := os.Open(pidFile)
-	if err != nil { return false, errors.New("PID file " + pidFile + " exist, but couldn't be open with error: " + err.Error()) }
+        // PID File Exist.
+        f, err := os.Open(pidFile)
+        if err != nil { return false, errors.New("PID file " + pidFile + " exist, but couldn't be open with error: " + err.Error()) }
 
-	fi, err := f.Stat()		
-	if err != nil { return false, errors.New("Couldn't run file stat command on PID file " + pidFile + " with error: " + err.Error() ) }
+        fi, err := f.Stat()
+        if err != nil { return false, errors.New("Couldn't run file stat command on PID file " + pidFile + " with error: " + err.Error() ) }
 
-	d := make( []byte, fi.Size() )
+        d := make( []byte, fi.Size() )
 
-	_, err = f.Read(d)
-	if err != nil { return false, errors.New("Pid file " + pidFile +" exist but couldn't read with error: " + err.Error() ) }
-		
-	pid, err := strconv.Atoi(string(d))
+        _, err = f.Read(d)
+        if err != nil { return false, errors.New("Pid file " + pidFile +" exist but couldn't read with error: " + err.Error() ) }
+
+        pid, err := strconv.Atoi(string(d))
         if err != nil { return false, errors.New("PID file is not the right format. Please delete PID file "+ pidFile +" manually.") }
-        
+
         process, err := os.FindProcess( pid )
-        
+
         if err != nil {
-		// Windows process not found
-		// If PID file is existed but process is not found then remove pid files.			
-		RemovePIDFile(pidFile)
-		return true, errors.New("Process was not found from PID file, thus PID file is removed.")
+            // Windows process not found
+            // If PID file is existed but process is not found then remove pid files.
+            RemovePIDFile(pidFile)
+            return true, errors.New("Process was not found from PID file, thus PID file is removed.")
         } else {
             err = process.Signal(syscall.Signal(0))
-			if err == nil {
-				// Linux process found
-				err2 = process.Signal(os.Interrupt)
-				if err2 != nil { return false, errors.New("Process couldn't be kill. PID file was not removed.") }
-				
-				RemovePIDFile(pidFile)
-				return true, nil
-			} else {
-				if err.Error() == "operation not permitted" {
-					// Linux process is found but not owned
-					return false, errors.New("Process is found, but is not owned. Thus couldn't be kill. PID file was not removed.")
-				}
-				
-				// Linux everything else process is not found
-				RemovePIDFile(pidFile)
-				return true, errors.New("Process was not found from PID file, thus PID file is removed.")
-			}
-			
-			// This part for windows where process is found.
-			
-			err2 = process.Signal(os.Interrupt)
-			if err2 != nil { return false, errors.New("Process couldn't be kill. PID file was not removed.") }
-	
-			RemovePIDFile(pidFile)
-	
-			return true, nil
-        }        
-    }   
+            if err == nil {
+                // Linux process found
+                err2 = process.Signal(os.Interrupt)
+                if err2 != nil { return false, errors.New("Process couldn't be kill. PID file was not removed.") }
+
+                RemovePIDFile(pidFile)
+                return true, nil
+            } else {
+                if err.Error() == "operation not permitted" {
+                    // Linux process is found but not owned
+                    return false, errors.New("Process is found, but is not owned. Thus couldn't be kill. PID file was not removed.")
+                }
+
+                // Linux everything else process is not found
+                RemovePIDFile(pidFile)
+                return true, errors.New("Process was not found from PID file, thus PID file is removed.")
+            }
+
+            // This part for windows where process is found.
+
+            err2 = process.Signal(os.Interrupt)
+            if err2 != nil { return false, errors.New("Process couldn't be kill. PID file was not removed.") }
+
+            RemovePIDFile(pidFile)
+
+            return true, nil
+        }
+    }
 }
