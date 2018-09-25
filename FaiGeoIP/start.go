339813cce53ac6a0c3e7ec5eefee06a941e83e49
kevinhng86 <kevinhng86@gmail.com>
2018-09-24 18:39:55 -0700

    Update start.go

--- FaiGeoIP/start.go
+++ FaiGeoIP/start.go
@@ -22,7 +22,7 @@
 #    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN            #
 #    THE SOFTWARE.                                                                        #
 #                                                                                         #
-#     CREDIT:                                                                              #
+#    CREDIT:                                                                              #
 #    A special thanks to Chirag Mehta                                                     #
 #    http://chir.ag/projects/geoiploc for the half division formula.                      #
 #                                                                                         #
